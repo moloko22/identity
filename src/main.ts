a622f39bcb2cec078a11c775c1314bc9df34eb84
Illia Tsapu <sonofzeus2205@gmail.com>
2024-09-06 17:56:21 -0600

    Fix data-source connection + DB

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,32 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+import AppDataSource from './data/data-source';
 
 const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-
-  await app.listen(PORT);
+  AppDataSource.initialize()
+    .then(() => {
+      console.log('Data Source has been initialized!');
+      return AppDataSource.runMigrations();
+    })
+    .then(() => {
+      console.log('Migrations have been run!');
+      app.useGlobalPipes(new ValidationPipe());
+      return app.listen(PORT);
+    })
+    .then(() => {
+      console.log(`Application is running on: http://localhost:${PORT}`);
+    })
+    .catch((error) => {
+      console.error(
+        'Error during Data Source initialization or migration execution',
+        error,
+      );
+      process.exit(1);
+    });
 }
+
 bootstrap();
