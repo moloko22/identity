6897ce9d4b33d6676666d3ee6647a9901068b0e5
Illia Tsapu <sonofzeus2205@gmail.com>
2025-01-18 23:42:38 -0700

    Get all users

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserRepository } from 'src/data/repositories/user.repository';
-import { GetUserByIdController } from './features/get-user-by-id/get-user-by-id.controller';
+import { GetUserByIdController } from './features/get-user-by-id/get-user-by-id';
 import { User } from './entity/user.entity';
+import { GetUsersController } from './features/get-users/get-users';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [GetUserByIdController],
+  controllers: [GetUserByIdController, GetUsersController],
   providers: [UserRepository],
 })
-export class UserModule {}
+export class UserModule { }
